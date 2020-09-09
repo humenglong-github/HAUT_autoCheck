644b6eeccf31ba14065b7e8232495761f87f092c
Wilfred-wei <70147411+Wilfred-wei@users.noreply.github.com>
2020-09-09 10:55:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import json
 import requests
 import random
-import os
+import datetime
 
 #sectets字段录入
 deptId = eval(input())
@@ -17,19 +17,18 @@ homeTown = input()
 personNum = input()
 homeAddress = input()
 local = input()
-sex = "男"
+
+
+
 area = {'address':homeAddress,'text':local,'code':""}
 areaStr = json.dumps(area, ensure_ascii=False)
 
-#随机温度(36.2~36.8)
-a=random.uniform(36.2,36.8)
-temperature = round(a, 1)
 
 sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
 
-json =  {
+jsons =  {
     "businessType": "epmpics",
-    "method": "submitUpInfo",
+    "method": "submitUpInfoSchool",
     "jsonData": {
         "deptStr": {
             "deptid": deptId,
@@ -40,134 +39,45 @@ json =  {
         "customerid": "43",
         "deptid": deptId,
         "source": "app",
-        "templateid": "pneumonia",
+        "templateid": "clockSign3",
         "stuNo": stuNum,
         "username": userName,
-        "phonenum": phoneNum,
         "userid": userId,
         "updatainfo": [
-            {
-                "propertyname": "isGoWarningAdress",
-                "value": sex
-            },
-            {
-                "propertyname": "jtdz",
-                "value": homeTown
-            },
-            {
-                "propertyname": "personNO",
-                "value": personNum
-            },
-            {
-                "propertyname": "langtineadress",
-                "value": homeAddress
-            },
-            {
-                "propertyname": "ownPhone",
-                "value": phoneNum
-            },
-            {
-                "propertyname": "emergencyContact",
-                "value": emergencyNum
-            },
-            {
-                "propertyname": "tradeNum",
-                "value": dormNum
-            },
-            {
-                "propertyname": "temperature",
-                "value": temperature
-            },
-            {
-                "propertyname": "symptom",
-                "value": "均无"
-            },
-            {
-                "propertyname": "isContactpatient",
-                "value": "均无"
-            },
-            {
-                "propertyname": "istouchcb",
-                "value": "否"
-            },
-            {
-                "propertyname": "isTransitProvince",
-                "value": "否"
-            },
-            {
-                "propertyname": "isTouch",
-                "value": "否"
-            },
-            {
-                "propertyname": "backadress",
-                "value": ""
-            },
-            {
-                "propertyname": "isContactFriendIn14",
-                "value": "否"
-            },
-            {
-                "propertyname": "sxaddress",
-                "value": ""
-            },
-            {
-                "propertyname": "medicalObservation",
-                "value": "否"
-            },
-            {
-                "propertyname": "sxss",
-                "value": ""
-            },
-            {
-                "propertyname": "isConfirmed",
-                "value": "否"
-            },
-            {
-                "propertyname": "assistRemark",
-                "value": ""
-            },
-            {
-                "propertyname": "gyfh",
-                "value": "否"
-            },
-            {
-                "propertyname": "FamilyIsolate",
-                "value": ""
-            },
-            {
-                "propertyname": "ishborwh",
-                "value": "否"
-            },
-            {
-                "propertyname": "IsHospitaltxt",
-                "value": ""
-            },
-            {
-                "propertyname": "fhhb",
-                "value": "否"
-            },
-            {
-                "propertyname": "isname",
-                "value": ""
-            },
-            {
-                "propertyname": "other1",
-                "value": ""
-            },
-            {
-                "propertyname": "isFFHasSymptom",
-                "value": "是"
-            }
+         {
+	"propertyname":"temperature:,
+	"value":"36.5"
+         },
+         {
+	"propertyname":"symptom",
+	"value":"无症状"
+         }
         ],
-        "gpsType": 1
+        "customerAppTypeRuleId":148,
+        "clockState":0
     }
 }                       
-response = requests.post(sign_url, json=json)
+#提交打卡
+response = requests.post(sign_url, json=jsons)
+utcTime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+cstTime = utcTime.strftime("%H:%M:%S")
 print(response.text)
-
+#结果判定
+if response.json()["msg"] == '成功':
+        msg = "打卡成功-" + cstTime
+else:
+        msg = "打卡异常-" + cstTime
+print(msg)
+#微信通知
 sckey = input()
-title = "打卡结果通知"
-content = response.text
+title = msg
+result = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
+content = f"""
+```
+{result}
+```
+### 😀[收藏](https://github.com/YooKing/HAUT_autoCheck)此项目
+"""
 data = {
 "text":title,
 "desp":content
